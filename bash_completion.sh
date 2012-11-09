42f091aad938d671629c66fc55d15685f9dc6f84
Olivier Favre <olivier@yakaz.com>
2012-11-09 11:24:22 +0100

    ant-nocolor alias using vanilla ant, with completion

--- bash_completion.sh
+++ bash_completion.sh
@@ -2,5 +2,6 @@
 type _ant >/dev/null 2>&1 || . /usr/share/bash-completion/completions/ant
 # Use it for our tool's completion
 complete -F _ant ant-color
+complete -F _ant ant-nocolor
 
 # vim: ts=2 sw=2 sts=2 et ft=sh
